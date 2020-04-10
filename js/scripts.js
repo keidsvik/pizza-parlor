d2a7ffeffaa71daadd69c5be0ce10444867a65a8
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-10 09:56:28 -0700

    add size related prices to business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 //Business logic
 
-function Pizza (toppings, size) {
+function Pizza (toppings, size, price) {
   this.toppings = toppings;
   this.size = size;
+  this.price = price;
 }
 
-Pizza.prototype.sizeLarge = function () {
-  return this + "13.99"
-};
+Pizza.prototype.pizzaPrice = function () {
+  if (this.size === 'small') {
+    this.price += 8;
+  } else if (this.size === "medium") {
+    this.price += 11;
+  } else {
+    this.price += 15;
+  }
+
+  return "Your" + this.toppings + "pizza is" + this.price;
+}
 
 
 
@@ -16,11 +25,11 @@ Pizza.prototype.sizeLarge = function () {
 
 // UI
 
-var myPizza = new Pizza ([toppings: any, size: any]);
+var myPizza = new Pizza ([this.toppings], this.size);
 
 $(document).ready(function(){
   $('form#createPizza').submit(function(event) {
     event.preventDefault();
     varjghg 
-  }
-}
+  })
+});
